423d11a1177b582fa4e18197b0489526e60e8f68
SupremeW1zard24 <2008-Arsen-2008@mail.ru>
2025-05-16 00:25:18 +0500

    Create CFG.py

---
+++ CFG.py
@@ -0,0 +1,5 @@
+API_TOKEN = "ТОКЕН БОТА"
+WEATHER_API_KEY = "ТОКЕН ПОГОДЫ"
+ADMIN_ID = "ID АДМИНА"  # Вставлять без кавычек
+DATABASE = "bot_db.sqlite"
+LOG_CHANNEL_ID = "ID ГРУППЫ"  # # Вставлять без кавычек
